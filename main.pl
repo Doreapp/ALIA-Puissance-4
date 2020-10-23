e37177d99289f2881baf1b6b52a667336e88fa65
Damien <48095764+DamienCarreau@users.noreply.github.com>
2020-10-23 10:54:29 +0200

    Update main.pl

--- main.pl
+++ main.pl
@@ -60,9 +60,9 @@ affiche([H|T],L) :- reverse(H,R), append(L,[R],L2), affiche(T,L2).
 
 afficheColonne(_,_,6).
 afficheColonne(L,7,B) :- nl, B1 is B+1, afficheColonne(L,0,B1).
-afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 = 1, write("X"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
-afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 = 2, write("O"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
-afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 = 0, write("*"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
+afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 == 1, write("X"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
+afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 == 2, write("O"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
+afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 == 0, write("."), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
 
 
 
