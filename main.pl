63c2117d5541c474262b093189685842fbc288ac
Doreapp <Doreapp@users.noreply.github.com>
2020-10-23 11:42:54 +0200

    fix move pour gagner

--- main.pl
+++ main.pl
@@ -18,14 +18,9 @@ gagnerColonne(J, [H|_]) :- sousliste([J,J,J,J], H).
 gagnerColonne(J, [_|T]) :- T \== [], gagnerColonne(J, T).
 
 % ** Retourne si le joueur J Ã  gagner sur la ligne Y
-gagnerLigne(J, Y, G):- maplist(element(Y),G,L), sousliste([J,J,J,J], L).
+gagnerLigne(J, Y, G):- maplist(nth1(Y),G,L), sousliste([J,J,J,J], L).
 
-gagnerLignes(J, G):- gagnerLigne(J, 1, G).
-gagnerLignes(J, G):- gagnerLigne(J, 2, G).
-gagnerLignes(J, G):- gagnerLigne(J, 3, G).
-gagnerLignes(J, G):- gagnerLigne(J, 4, G).
-gagnerLignes(J, G):- gagnerLigne(J, 5, G).
-gagnerLignes(J, G):- gagnerLigne(J, 6, G).
+gagnerLignes(J, G):- gagnerLigne(J, Y, G), Y>0, Y<7.
 
 % Regarder si le joueur J a gagnÃƒÂ©
 % 1. Les colonnes
@@ -37,18 +32,24 @@ gagner(J,G):- gagnerLignes(J,G).
 % 3.1. Recherche les diagonales (type \) dans G
 gagner(J,G):- sousliste([C1,C2,C3,C4], G), % Récup 4 colonnes
 		   element(I1,C1,J), % qui contiennent J
+                   I2 is I1+1,
                    element(I2,C2,J),
+                   I3 is I2+1,
 		   element(I3,C3,J),
-		   element(I4,C4,J),
-                   I2 is I1+1, I3 is I2+1, I4 is I3+1. % Et chacun sont sur une même diagonale \
+                   I4 is I3+1,
+		   element(I4,C4,J).
+                   % Et chacun sont sur une même diagonale \
 
 % 3.2. Recherche les diagonales (type /) dans G
 gagner(J,G):- sousliste([C1,C2,C3,C4], G), % Récup 4 colonnes
 		   element(I1,C1,J), % qui contiennent J
+                   I2 is I1-1,
                    element(I2,C2,J),
+                   I3 is I2-1,
 		   element(I3,C3,J),
-		   element(I4,C4,J),
-                   I2 is I1-1, I3 is I2-1, I4 is I3-1. % Et chacun sont sur une même diagonale /
+                   I4 is I3-1,
+		   element(I4,C4,J).
+                   % Et chacun sont sur une même diagonale /
 
 % Affiche La grille L
 affiche([],L) :- afficheColonne(L,0,0).
@@ -56,9 +57,9 @@ affiche([H|T],L) :- reverse(H,R), append(L,[R],L2), affiche(T,L2).
 
 afficheColonne(_,_,6).
 afficheColonne(L,7,B) :- nl, B1 is B+1, afficheColonne(L,0,B1).
-afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 = 1, write("X"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
-afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 = 2, write("O"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
-afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 = 0, write("*"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
+afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 == 1, write("X"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
+afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 == 2, write("O"), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
+afficheColonne([[H1|T1]|T],A,B) :- H1\==[],H1 == 0, write("."), append(T,[T1],L), A1 is A+1, afficheColonne(L,A1,B).
 
 
 
@@ -70,7 +71,7 @@ compter([X|T],N) :- X \== 0, compter(T,N).
 % Ajoute en fin de colonne
 ajouterEnFin(X,[0|T],[X|T]).
 ajouterEnFin(X,[],[X]).
-ajouterEnFin(X,[H|L1],[H|L2]):- ajouterEnFin(X,L1,L2).
+ajouterEnFin(X,[H|L1],[H|L2]):- H\==0, ajouterEnFin(X,L1,L2).
 
 % Essaie d'ajouter l'Ã©lÃ©ment X ÃƒÂ  la colonne C
 ajouter(C,_,C) :- compter(C,0).
@@ -85,12 +86,19 @@ lancerJeu(_) :- G=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,
 
 heuristique1(G) :- gagner(1,G).
 heuristique1(G) :- gagner(2,G).
-heuristique1(G) :- heuristiqueRandom(1, G, G1),heuristique2(G1).
+heuristique1(G) :- movePourGagner(1, G, G1),
+    write("1 joue pour gagner : "), nl,
+    affiche(G1,[]).
+heuristique1(G) :- heuristiqueRandom(1, G, G1),
+    heuristique2(G1).
 % heuristique1(G) :- movePourGagner(1, G, G1, 1), write("1 joue pour
 % gagner"), heuristique2(G1).
 %
 heuristique2(G) :- gagner(1,G).
 heuristique2(G) :- gagner(2,G).
+heuristique2(G) :- movePourGagner(2, G, G1),
+    write("2 joue pour gagner : "), nl,
+    affiche(G1,[]).
 heuristique2(G) :- heuristiqueRandom(2, G, G1),heuristique1(G1).
 
 jouerJoueur1(G) :- write("Joue, J1 :"), read(L), nth1(L,G,C), ajouter(C, 1, C1), changeColonne(G,L,C1,[],1,G1), affiche(G1,[]), heuristique2(G1). % gagnant(), jouerJoueur2().
@@ -109,16 +117,13 @@ joueRandom(Joueur, Grille, Index, Count, Colonne, Grille1) :- Count\==0,
     affiche(Grille1,[]).
 joueRandom(Joueur, Grille, _, 0, _, Grille1) :- heuristiqueRandom(Joueur, Grille, Grille1).
 
-
-
 % Crée la grille G1 à partir de G dans laquelle le joueur J à joué la
 % colonne L, si possible.
 jouerMove(J, G, L, G1) :- nth1(L,G,C), compter(C,Y), Y\==0, ajouter(C, J, C1), changeColonne(G,L,C1,[],1,G1).
 
 % la colonne C ferrai gagner le joueur J joue un move pour gagner si possible
-movePourGagner(J, G, G1, N) :- jouerMove(J, G, N, G1), gagner(J, G1).
-movePourGagner(J, G, G1, N) :-  N1 is N+1, jouerMove(J, G, N1, G1), gagner(J, G1).
-
+movePourGagner(Joueur, Grille, Grille1) :- jouerMove(Joueur, Grille, _, Grille1),
+    gagner(Joueur, Grille1).
 
 
 
