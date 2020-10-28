16303b0da8f45208f581f5b75c981d2407cef422
Alexandre Bonhomme <alexandre.bonhomme@insa-lyon.fr>
2020-10-28 12:18:59 +0100

    Ajout de minMax avec anticipation

--- main.pl
+++ main.pl
@@ -58,7 +58,7 @@ afficherGagnant(J):- write("Le joueur "),write(J),write(" a gagne"),nl.
 
 
 % Affiche La grille L
-affiche([],L) :- afficheColonne(L,0,0).
+affiche([],L) :- afficheColonne(L,0,0),nl,nl,nl,nl,nl.
 affiche([H|T],L) :- reverse(H,R), append(L,[R],L2), affiche(T,L2).
 
 afficheColonne(_,_,6).
@@ -88,6 +88,23 @@ changeColonne([],_,_,G1,8,G1).
 changeColonne([H|T], X, C, G1, N,Q) :- N \== X, append(G1,[H],G2), N1 is N+1, changeColonne(T,X,C,G2,N1,Q).
 changeColonne([_|T], X, C, G1, X,Q) :- append(G1,[C],G2), N1 is X+1, changeColonne(T,X,C,G2,N1,Q).
 
+%test maxList
+max_l([X],X) :- !, true.
+max_l([X|Xs], M):- max_l(Xs, M), M >= X.
+max_l([X|Xs], X):- max_l(Xs, M), X >  M.
+
+% heuristque min-max statique
+minmaxStatique(JoueurJouant,Grille,Grille1) :- MMS=[[3,4,5,5,4,3,-1],[4,6,8,8,6,4,-1],[5,8,11,11,8,5,-1],[7,10,13,13,10,7,-1],[5,8,11,11,8,5,-1],[4,6,8,8,6,4,-1],[3,4,5,5,4,3,-1]], heuristiqueMMS(Grille,_,MMS,Grille1,JoueurJouant).
+
+%G la grille du jeu, L la ligne MinMax jouable, MMS le tableau MinMaxStatique
+heuristiqueMMS(G,L,MMS,G1,JoueurJouant) :- length(L,T), T < 7,J is T+1, %Trouve l'indice de la colone sur laquelle on travail
+                                        joueurOppose(JoueurJouant,JoueurOppose),jouerMove(JoueurJouant, G, J, G2),testMovePourGagner(JoueurOppose, G2,_), %Vérifie si jouer dans cette colonne offre un coup gagnant
+                                        nth1(J,G,C), compter(C,N) ,nth1(J,MMS,Cbis),I is 7-N,nth1(I,Cbis,E), %Trouve la case du tableau MMS correspondant
+                                        ajouterEnFin(E,L,L1),heuristiqueMMS(G,L1,MMS,G1,JoueurJouant). %Ajoute la valeur au tableau, refais un appel a lui meme
+heuristiqueMMS(G,L,MMS,G1,JoueurJouant) :- length(L,T), T < 7,ajouterEnFin(-1,L,L1),heuristiqueMMS(G,L1,MMS,G1,JoueurJouant).
+heuristiqueMMS(G,L,_,G1,JoueurJouant) :- max_l(L,X),nth1(I,L,X),jouerMove(JoueurJouant, G, I, G1).
+
+
 lancerJeu(_) :- G=[[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]], affiche(G,[]), heuristique1(G). % ajouter l'appel à la premiÃ¨re heuristique
 
 heuristique1(G) :- gagner(1,G).
@@ -97,18 +114,24 @@ heuristique1(G) :- movePourGagner(1, G, G1),
     write("1 joue pour gagner : "), nl,
     affiche(G1,[]),
     afficherGagnant(1).
+
 heuristique1(G) :- movePourEmpecherGagner(1, 2, G, G1),
     write("1 joue pour empecher de gagner 2 : "), nl,
     affiche(G1,[]),
     heuristique2(G1).
 
-heuristique1(G) :- heuristiqueRandomAvecAnticipation(1,G,G1),
-    write("RandomAvecAnticipation (1) joue"), nl,
-    affiche(G1,[]),
-    heuristique2(G1).
+%heuristique1(G) :- heuristiqueRandomAvecAnticipation(1,G,G1),
+%    write("RandomAvecAnticipation (1) joue"), nl,
+%    affiche(G1,[]),
+%    heuristique2(G1).
 
-heuristique1(G) :- heuristiqueRandom(1, G, G1),
-    write("Random (2) joue "), nl,
+%heuristique1(G) :- heuristiqueRandom(1, G, G1),
+%    write("Random (2) joue "), nl,
+%    affiche(G1,[]),
+%    heuristique2(G1).
+
+heuristique1(G) :- minmaxStatique(1, G, G1),
+    write("MinMax (1) joue "), nl,
     affiche(G1,[]),
     heuristique2(G1).
 
@@ -134,6 +157,7 @@ heuristique2(G) :- heuristiqueRandom(2, G, G1),
     affiche(G1,[]),
     heuristique1(G1).
 
+
 jouerJoueur1(G) :- write("Joue, J1 :"), read(L), nth1(L,G,C), ajouter(C, 1, C1), changeColonne(G,L,C1,[],1,G1), affiche(G1,[]), heuristique2(G1). % gagnant(), jouerJoueur2().
 
 jouerJoueur2(G) :- write("Joue, J2 :"), read(L), nth1(L,G,C), ajouter(C, 2, C1), changeColonne(G,L,C1,[],1,G1), affiche(G1,[]), heuristique1(G1).
@@ -147,7 +171,7 @@ joueRandom(Joueur, Grille, Index, Count, Colonne, Grille1) :- Count\==0,
     changeColonne(Grille,Index,Colonne1,[],1,Grille1).
 joueRandom(Joueur, Grille, _, 0, _, Grille1) :- heuristiqueRandom(Joueur, Grille, Grille1).
 
-% joue un coup random qui ne mene pas � la victoire de l'adversaire
+% joue un coup random qui ne mene pas � la victoire de l'adversaire
 heuristiqueRandomAvecAnticipation(Joueur, Grille, Grille1) :- joueurOppose(Joueur, JoueurOp), heuristiqueRandom(Joueur, Grille, Grille1), testMovePourGagner(JoueurOp, Grille1, _).
 
 test(Joueur, Grille, Grille1) :- joueurOppose(Joueur, JoueurOp), nth1(3,Grille,Colonne), compter(Colonne,Count), joueRandom(Joueur, Grille, 3, Count, Colonne, Grille1) , testMovePourGagner(JoueurOp, Grille1, X), X == 0.
